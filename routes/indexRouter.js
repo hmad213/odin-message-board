b150350c0a666cf607892868a0ffb7a0751bc106
hmad213 <142343785+hmad213@users.noreply.github.com>
2026-03-05 14:22:52 +0500

    Add a post method for the new route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -22,4 +22,13 @@ indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
 
+indexRouter.post("/new", (req, res) => {
+  const name = req.body.name;
+  const text = req.body.text;
+
+  messages.push({ text: text, user: name, added: new Date() });
+
+  res.redirect("/");
+});
+
 module.exports = indexRouter;
