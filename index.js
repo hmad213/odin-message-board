8c4d8085f63c6cec7ddb6eb863867df0bb4bcd17
hmad213 <142343785+hmad213@users.noreply.github.com>
2026-03-05 13:54:00 +0500

    Add index route

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const PORT = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000;
+const indexRouter = require("./routes/indexRouter");
+
+app.use("/", indexRouter);
 
 app.use((req, res) => {
-    res.send("Hello world");
-})
+  res.send("Hello world");
+});
 
 app.listen(PORT, (error) => {
-    if(error){
-        throw error;
-    }
+  if (error) {
+    throw error;
+  }
 
-    console.log("Listening on port " + PORT);
-})
+  console.log("Listening on port " + PORT);
+});
